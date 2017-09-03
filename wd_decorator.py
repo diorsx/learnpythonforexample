e7d96e7ca3cd5673296dcab123ad9d9227f5ca37
diorsx <690717566@qq.com>
2017-09-03 23:38:40 +0800

    Added decorator for examples

---
+++ wd_decorator.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+Author: wood
+Date: 2017-08-28
+Desc: 实现python的装饰器
+'''
+
+import functools
+import time
+import logging
+from threading import Thread
+import signal
+
+#构建不带参数的装饰器1
+def logging_process(func):
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        print("start called: %s" % func.__name__)
+        result = func(*args, **kwargs)
+        print("end called: %s" % func.__name__)
+        return result
+    return decorator
+
+#构建不带参数的装饰器2
+def fn_timer(func):
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        t1 = time.time()
+        print "Total time running %s: %s seconds" %(func.__name__, t1-t0)
+        return result
+    return decorator
+
+#同时使用装饰器
+@logging_process
+@fn_timer
+def get_response(url="http://www.baidu.com"):
+    import requests
+    try:
+        r = requests.get(url)
+        print u"请求返回码：%s" %r.status_code
+    except Exception, e:
+        print "error: %s" %e
+
+        
+#构建带参数的装饰器1
+def use_logging(level='warn'):
+    if level == 'warn':
+        print_log = logging.warn
+    elif level == 'error':
+        print_log = logging.error
+    else:
+        print_log = logging.info
+    def _outer(func):
+        @functools.wraps(func)
+        def _inner(*args, **kwargs):
+            print_log("Start {0} at {1}".format(func.__name__, time.ctime()))
+            result = func(*args, **kwargs)
+            print_log("End {0} at {1}".format(func.__name__, time.ctime()))
+        return _inner
+    return _outer
+    
+#构建带参数的装饰器2
+def time_limit(interval):
+    def _outer(func):
+        def handler():
+            raise RuntimeError()
+        def _inner(*args, **kwargs):
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(interval)
+            res = func(*args, **kwargs)
+            signal.alarm(0)
+            return res
+        return _inner
+    return _outer
+    
+#使用带参数的装饰器
+@use_logging(level='warn')
+def get_response(url="http://www.baidu.com"):
+    import requests
+    try:
+        r = requests.get(url)
+        print u"请求返回码：%s" %r.status_code
+    except Exception, e:
+        print "error: %s" %e
+get_response()
+
+#使用带参数的装饰器
+@time_limit(5)
+def get_response(url="http://www.baidu.com"):
+    import requests
+    try:
+        r = requests.get(url)
+        print u"请求返回码：%s" %r.status_code
+    except Exception, e:
+        print "error: %s" %e
+get_response()
+

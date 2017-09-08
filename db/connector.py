a403dbe861ea31519bba0a1b2a9cd0f862e3bf4d
PC-201511161410\Administrator <690717566@qq.com>
2017-09-08 19:57:43 +0800

    db connector

---
+++ db/connector.py
@@ -0,0 +1,312 @@
+#-*- coding: utf-8 -*-
+'''
+Auther: Wood
+Date: 2017-09-06
+Desc: mysql连接器的实现
+'''
+
+import time, uuid, functools, threading, logging
+
+# global engine object:
+engine = None    
+
+#扩展py中字典类型
+class Dict(dict):
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+#定义异常        
+class DBError(Exception):
+    pass
+
+class MultiColumnsError(DBError):
+    pass
+
+class _LazyConnection(object):
+
+    '''
+    管理数据库连接生命期，从engine获取连接
+    cursor: 连接的游标
+    commit: 提交修改
+    rollback: 回滚
+    cleanup: 清理连接
+    '''
+
+    def __init__(self):
+        self.connection = None
+
+    def cursor(self):
+        if self.connection is None:
+            connection = engine.connect()
+            logging.info('open connection <%s>...' % hex(id(connection)))
+            self.connection = connection
+        return self.connection.cursor()
+
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
+
+    def cleanup(self):
+        if self.connection:
+            connection = self.connection
+            self.connection = None
+            logging.info('close connection <%s>...' % hex(id(connection)))
+            connection.close()
+  
+class _Engine(object):
+
+    '''
+    property:
+        _connect: 数据连接的抽象
+    method:
+        connect: 执行数据库连接方法
+    '''
+    def __init__(self, connect):
+        self._connect = connect
+
+    def connect(self):
+        return self._connect()  
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    import mysql.connector
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+    params = dict(user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+    params.update(kw)
+    params['buffered'] = True
+    engine = _Engine(lambda: mysql.connector.connect(**params))
+    # test connection...
+    logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))        
+        
+'''
+创建一个threadlocal 对象，对于不同的线程，使用不同的数据库链接
+'''  
+class _DbCtx(threading.local):
+    '''
+    连接信息保存在此对象中
+    '''
+    def __init__(self):
+        self.connection = None
+        self.transactions = 0
+
+    def is_init(self):
+        return not self.connection is None
+
+    def init(self):
+        logging.info('open lazy connection...')
+        self.connection = _LazyConnection()
+        self.transactions = 0
+
+    def cleanup(self):
+        self.connection.cleanup()
+
+    def cursor(self):
+        return self.connection.cursor()
+
+#thread-local变量
+_db_ctx = _DbCtx()
+        
+class _ConnectionCtx(object):
+    '''
+    _ConnectionCtx object that can open and close connection context. _ConnectionCtx object can be nested and only the most 
+    outer connection has effect
+    '''
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+def connection():
+    '''
+    Return _ConnectionCtx object that can be used by 'with' statement
+    '''
+    return _ConnectionCtx()
+
+def with_connection(func):
+    '''
+    Decorator for reuse connection
+    '''
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _wrapper
+
+class _TransactionCtx(object):
+    '''
+    _TransactionCtx object that can handle transactions.
+    '''
+
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            # needs open a connection first:
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transaction...' if _db_ctx.transactions==1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions==0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        logging.info('commit transaction...')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback transaction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
+def transaction():
+    '''
+    Create a transaction object so can use with statement:
+    '''
+    return _TransactionCtx()
+
+def with_transaction(func):
+    '''
+    Decorator that makes function around transaction.
+    '''
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        _start = time.time()
+        with _TransactionCtx():
+            return func(*args, **kw)
+        _profiling(_start)
+    return _wrapper
+
+def _select(sql, first, *args):
+    'execute select SQL and return unique result or list results.'
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+@with_connection
+def select_one(sql, *args):
+    '''
+    Execute select SQL and expected one result. 
+    If no result found, return None.
+    If multiple results found, the first one returned.
+    '''
+    return _select(sql, True, *args)
+
+@with_connection
+def select_int(sql, *args):
+    '''
+    Execute select SQL and expected one int and only one int result. 
+    MultiColumnsError: Expect only one column.
+    '''
+    d = _select(sql, True, *args)
+    if len(d)!=1:
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+@with_connection
+def select(sql, *args):
+    '''
+    Execute select SQL and return list or empty list if no result.
+    '''
+    return _select(sql, False, *args)
+
+@with_connection
+def _update(sql, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        r = cursor.rowcount
+        if _db_ctx.transactions==0:
+            # no transaction enviroment:
+            logging.info('auto commit')
+            _db_ctx.connection.commit()
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+def insert(table, **kw):
+    '''
+    Execute insert SQL.
+    IntegrityError: 1062 (23000): Duplicate entry '2000' for key 'PRIMARY'
+    '''
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+def update(sql, *args):
+    '''
+    Execute update SQL.
+    '''
+    return _update(sql, *args)
+
+    
+if __name__ == '__main__':
+    create_engine(user='radius', password='feeyo123', database='FYVPN', host='192.168.5.12', port=3306)
+    r = select_int('select count(*) from radgroupreply')
+    print r
+        
---
+++ db/field.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+'''
+Auther: Wood
+Date: 2017-09-06
+Desc: mysql连接器的实现
+'''
+
+import time, logging
+
+class Field(object):
+    '''
+    映射为表中的字段
+    '''
+    _count = 0
+    def __init__(self, **kw):
+        self.name = kw.get('name', None)
+        self._default = kw.get('default', None)
+        self.primary_key = kw.get('primary_key', False)
+        self.nullable = kw.get('nullable', False)
+        self.updatable = kw.get('updatable', True)
+        self.insertable = kw.get('insertable', True)
+        self.ddl = kw.get('ddl', '')
+        self._order = Field._count
+        Field._count = Field._count + 1
+
+    @property
+    def default(self):
+        d = self._default
+        return d() if callable(d) else d
+
+    def __str__(self):
+        s = ['<%s:%s,%s,default(%s),' % (self.__class__.__name__, self.name, self.ddl, self._default)]
+        self.nullable and s.append('N')
+        self.updatable and s.append('U')
+        self.insertable and s.append('I')
+        s.append('>')
+        return ''.join(s)
+
+class StringField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'varchar(255)'
+        super(StringField, self).__init__(**kw)
+
+class IntegerField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = 0
+        if not 'ddl' in kw:
+            kw['ddl'] = 'bigint'
+        super(IntegerField, self).__init__(**kw)
+
+class FloatField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = 0.0
+        if not 'ddl' in kw:
+            kw['ddl'] = 'real'
+        super(FloatField, self).__init__(**kw)
+
+class BooleanField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = False
+        if not 'ddl' in kw:
+            kw['ddl'] = 'bool'
+        super(BooleanField, self).__init__(**kw)
+
+class TextField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'text'
+        super(TextField, self).__init__(**kw)
+
+class BlobField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'blob'
+        super(BlobField, self).__init__(**kw)
+
+class VersionField(Field):
+
+    def __init__(self, name=None):
+        super(VersionField, self).__init__(name=name, default=0, ddl='bigint')
